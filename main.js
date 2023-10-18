2a60a8d392b5f9273d084a295b705e780b2ec2ef
David Törnros <david@dxgruppen.se>
2023-10-18 22:33:42 +0200

    lots of uppdates

---
+++ main.js
@@ -0,0 +1,82 @@
+document.addEventListener("DOMContentLoaded", function(e) {
+    document.querySelector("#contactCreate").addEventListener("click",function (event) {
+        let name,tel;
+        name=document.querySelector("#inputName").value;
+        tel=document.querySelector("#inputTel").value;
+        contactListAdd(name,tel);
+    });
+
+    document.querySelector(".validate-name").addEventListener("input",function (event) {
+        console.log(event);
+        validate(event.target,"name");
+    });
+
+    document.querySelector(".validate-tel").addEventListener("input",function (event) {
+        validate(event.target,"tel");
+    });
+});
+
+// Function: Load contactlist from local storage
+function contactListLoad() {
+
+}
+
+// Function: Save contactlist to local storage
+function contactListSave() {
+
+}
+
+// Function: Add contact to list
+function contactListAdd(name,tel) {
+
+}
+// Function: Delete contact from list
+function contactListDelete() {
+    
+}
+
+// Function: Delete all contacts
+function contactListClear() {
+    
+}
+// Function: Edit contact in list
+function contactListEdit() {
+    
+}
+
+
+function validate(element,type) {
+    let valid = true;
+    let value = element.value;
+    let errorMessage="";
+    let pattern="";
+    switch (type){
+        case "tel":
+            pattern=/^(0|\+)[0-9+-]{8,}$/;
+            errorMessage ="Ej giltligt telefonnummer";
+        break;
+        case "name":
+            pattern =/^[\p{L}\s'\-]{3,}$/u;
+            errorMessage ="Ej giltligt namn";
+        break;
+    }
+
+    // Test the input value with regular expression
+    valid = pattern.test(value);
+
+
+    if (valid) {
+        element.classList.remove("border-danger");
+        element.classList.remove("focus-ring-danger");
+        element.classList.add("focus-ring-success");
+        element.classList.add("border-success");
+    } else {
+        element.classList.remove("border-success");
+        element.classList.remove("focus-ring-success");
+        element.classList.add("focus-ring-danger");
+        element.classList.add("border-danger");
+    }
+
+    console.log(element);
+    return valid;
+}
