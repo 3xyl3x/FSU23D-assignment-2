ea9b8915eed19cf341c957ea6935087fbe09fd2f
David Törnros <david@dxgruppen.se>
2023-10-19 18:40:48 +0200

    clear list

--- main.js
+++ main.js
@@ -1,16 +1,18 @@
-document.addEventListener("DOMContentLoaded", function(e) {
+document.addEventListener("DOMContentLoaded", function (e) {
     contactListLoad();
 
     document.querySelector("#contactCreate").addEventListener("click", handleContactCreate);
     
+    document.querySelector("#contactsClear").addEventListener("click", contactListClear);
+
     document.body.addEventListener('input', function (event) {
         if (event.target.classList.contains('validate')) {
             validate(event.target);
         }
     });
-    
-    // Add listener to contactList since list elements are dynamicly added
-    document.getElementById('contactList').addEventListener('click', function(event) {
+
+    // Add listener to contactList since list elements are dynamically added
+    document.getElementById('contactList').addEventListener('click', function (event) {
         const contactElement = event.target.closest('li');
         if (event.target.classList.contains('contactEdit')) {
             handleContactEdit(contactElement);
@@ -21,9 +23,8 @@ document.addEventListener("DOMContentLoaded", function(e) {
 });
 
 function handleContactDelete(contactElement) {
-    // Could do a modal prompt here before calling contactList Delete
+    // Could do a modal prompt here before calling contactListDelete
     contactListDelete(contactElement);
-
 }
 
 function handleContactEdit(contactElement) {
@@ -37,22 +38,21 @@ function handleContactEdit(contactElement) {
         contactEditButton.innerHTML = 'Spara';
         inputFields.forEach(input => (input.disabled = false));
     } else if (contactEditMode === 'save') {
-        let errors;
-        const nameElement=contactElement.querySelector(".inputEditName");
-        const telElement=contactElement.querySelector(".inputEditTel");
+        const nameElement = contactElement.querySelector(".inputEditName");
+        const telElement = contactElement.querySelector(".inputEditTel");
         const alertElement = contactElement.querySelector('.contactEditAlert');
         validate(telElement);
         validate(nameElement);
-        errors=[];
-    
-        if (nameElement.getAttribute("data-validated")!=="true"){
+        const errors = [];
+
+        if (nameElement.getAttribute("data-validated") !== "true") {
             errors.push("Ange ett korrekt namn!");
         }
-        if (telElement.getAttribute("data-validated")!=="true"){
+        if (telElement.getAttribute("data-validated") !== "true") {
             errors.push("Ange ett korrekt telefonnummer!");
         }
 
-         if(errors.length===0){
+        if (errors.length === 0) {
             // Hide error element
             alertElement.classList.add("d-none");
             contactEditButton.setAttribute('data-mode', 'edit');
@@ -61,87 +61,82 @@ function handleContactEdit(contactElement) {
             inputFields.forEach(input => (input.disabled = true));
 
             // Clear input borders
-            inputSetBorder(nameElement,"normal");
-            inputSetBorder(telElement,"normal");
-            
-         } else {
+            inputSetBorder(nameElement, "normal");
+            inputSetBorder(telElement, "normal");
+
+        } else {
             // Show error element
             alertElement.classList.remove("d-none");
             // Output errors
-            alertElement.innerHTML=errors.join("<br>");
-         }
+            alertElement.innerHTML = errors.join("<br>");
+        }
     }
 }
 
 function handleContactCreate() {
-    let errors,alertElement;
-    const nameElement=document.querySelector("#inputName");
-    const telElement=document.querySelector("#inputTel");
+    const nameElement = document.querySelector("#inputName");
+    const telElement = document.querySelector("#inputTel");
     validate(telElement);
     validate(nameElement);
-    alertElement = document.querySelector('#contactCreateAlert');
-    errors=[];
+    const alertElement = document.querySelector('#contactCreateAlert');
+    const errors = [];
 
-    if (nameElement.getAttribute("data-validated")!=="true"){
+    if (nameElement.getAttribute("data-validated") !== "true") {
         errors.push("Ange ett korrekt namn!");
     }
-    if (telElement.getAttribute("data-validated")!=="true"){
+    if (telElement.getAttribute("data-validated") !== "true") {
         errors.push("Ange ett korrekt telefonnummer!");
     }
 
     // No errors
-    if(errors.length===0){
+    if (errors.length === 0) {
         // Add the contact
-        contactListAdd(nameElement.value,telElement.value);
+        contactListAdd(nameElement.value, telElement.value);
         // Hide error element
         alertElement.classList.add("d-none");
 
         // Clear input values
-        nameElement.value="";
-        telElement.value="";
+        nameElement.value = "";
+        telElement.value = "";
 
         // Clear input borders
-        inputSetBorder(nameElement,"normal");
-        inputSetBorder(telElement,"normal");
+        inputSetBorder(nameElement, "normal");
+        inputSetBorder(telElement, "normal");
 
         // clear validation
-        nameElement.setAttribute("data-validated",false);
-        telElement.setAttribute("data-validated",false);
+        nameElement.setAttribute("data-validated", false);
+        telElement.setAttribute("data-validated", false);
     } else {
         // Show error element
         alertElement.classList.remove("d-none");
         // Output errors
-        alertElement.innerHTML=errors.join("<br>");
+        alertElement.innerHTML = errors.join("<br>");
     }
-    
 }
 
-
-
-
-// Function: Load contactlist from local storage
+// Function: Load contact list from local storage
 function contactListLoad() {
-    console.log("Load list from localstorage");
+    console.log("Load list from local storage");
 }
 
-// Function: Save contactlist to local storage
+// Function: Save contact list to local storage
 function contactListSave() {
-    console.log("Save list to localstorage");
+    console.log("Save list to local storage");
 }
 
 // Function: Add contact to list
-function contactListAdd(name,tel) {
+function contactListAdd(name, tel) {
     // Get contactList element
-    let contactList = document.getElementById("contactList");
-    // Create a clone of contact element
-    let clonedElement = document.getElementById("contactElementTemplate").cloneNode(true);
+    const contactList = document.getElementById("contactList");
+    // Create a clone of the contact element
+    const clonedElement = document.getElementById("contactElementTemplate").cloneNode(true);
 
     // Make it visible by removing hide-class
     clonedElement.classList.add("contactElement");
 
     // Make it visible by removing hide-class
     clonedElement.classList.remove("d-none");
-    
+
     // Add name and telephone to the inputs
     clonedElement.querySelector(".inputEditName").value = name;
     clonedElement.querySelector(".inputEditTel").value = tel;
@@ -166,72 +161,85 @@ function contactListDelete(contactElement) {
     contactListSave();
 
     // Hide clear button if no more contacts
-    if (document.querySelectorAll('.contactElement').length===0)
-    document.querySelector("#contactsClear").classList.add("d-none");
+    if (document.querySelectorAll('.contactElement').length === 0)
+        document.querySelector("#contactsClear").classList.add("d-none");
 }
 
 // Function: Delete all contacts
 function contactListClear() {
+    // Implement this function if needed
+    const contactList = document.getElementById("contactList");
+    
+    // Remove all contact elements
+    const contactElements = contactList.querySelectorAll('.contactElement');
+    contactElements.forEach(contactElement => contactElement.remove());
+    
+    // Save list
+    contactListSave();
 
+    // Hide the clear button
+    document.querySelector("#contactsClear").classList.add("d-none");
 }
+
 // Function: Edit contact in list
 function contactListEdit() {
+    // Implement this function if needed
 
     // Save list
-    contactListSave();   
+    contactListSave();
 }
 
-
 function validate(element) {
-    type=element.dataset.validationType;
+    const type = element.dataset.validationType;
     let valid = true;
-    let value = element.value;
-    let pattern="";
-    switch (type){
-        case "tel":
-            pattern=/^(0|\+)[0-9+-]{8,}$/;
+    const value = element.value;
+    let pattern = "";
 
-        break;
+    switch (type) {
+        case "tel":
+            pattern = /^(0|\+)[0-9+-]{8,}$/;
+            break;
         case "name":
-            pattern =/^[\p{L}\s'\-]{3,}$/u;
-        break;
+            pattern = /^[\p{L}\s'\-]{3,}$/u;
+            break;
     }
 
-    // Test the input value with regular expression
+    // Test the input value with a regular expression
     valid = pattern.test(value);
 
-
     if (valid) {
-        element.setAttribute("data-validated",true);
-        inputSetBorder(element,"success");
+        element.setAttribute("data-validated", true);
+        inputSetBorder(element, "success");
     } else {
-        element.setAttribute("data-validated",false); 
-        inputSetBorder(element,"danger");
+        element.setAttribute("data-validated", false);
+        inputSetBorder(element, "danger");
     }
 
-    console.log(`validating input. [value=${value}]  [valid=${valid}]`);
+    console.log(`Validating input. [value=${value}]  [valid=${valid}]`);
     return valid;
 }
 
-function inputSetBorder(inputElement,borderType) {
+
+// Function
+function inputSetBorder(inputElement, borderType) {
     switch (borderType) {
         case "normal":
             inputElement.classList.remove("border-success");
             inputElement.classList.remove("focus-ring-success");
             inputElement.classList.remove("border-danger");
             inputElement.classList.remove("focus-ring-danger");
-        break;
+            break;
         case "danger":
             inputElement.classList.remove("border-success");
             inputElement.classList.remove("focus-ring-success");
             inputElement.classList.add("focus-ring-danger");
             inputElement.classList.add("border-danger");
-        break;
+            break;
         case "success":
             inputElement.classList.remove("border-danger");
             inputElement.classList.remove("focus-ring-danger");
             inputElement.classList.add("focus-ring-success");
             inputElement.classList.add("border-success");
-        break;
+            break;
     }
 }
